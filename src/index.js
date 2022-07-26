6f3ee1875e56a4b3fba78e30b88da5399902abca
Amanda Sakuma <amandasakuma@gmail.com>
2022-07-26 11:42:19 -0400

    oof

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+
+    const ramenMenu = document.querySelector('#ramen-menu')
+    let RamenData = []
+
+    fetch('http://localhost:3000/ramens')
+        .then(res => res.json())
+        .then(RamenData => console.log(RamenData))
+        //ERROR -- ramenData is being logged, but images aren't being rendered
+        .then(ramenData =>
+            ramenData.forEach((image) => renderImages(image)
+            )
+        )
+
+    //DISPLAY RAMEN PHOTOS
+    function renderImages(image) {
+
+        let ramenPhotoContainer = document.querySelector('#ramen-menu');
+        let img = document.createElement('img')
+        img.setAttribute('src', ramen.image)
+
+        //ADD EVENT LISTENER TO IMG
+        //ERROR -- Incorrectly pulling from object array. Also need to return values
+        img.addEventListener('click', function (e) {
+            console.log({
+                rating: e.target.ramens.rating.value,
+                comment: e.target.ramens.rating.value,
+            })
+        })
+        ramenPhotoContainer.append(image)
+    }
+    //DELIVERABLES -
+    //create new ramen
+    //I ran out of time
+    function newRamen(ramen) {
+        const form = form.document.getElementById("#new-ramen");
+
+        form.addEventListener('submit', e => {
+            e.preventDefaullt()
+
+            let img = document.createElement('img')
+            let p = document.createElement('p')
+
+
+        })
+        ramenMenu.append(newRamen)
+    }
+
+
+
+    renderImages();
+})
+
+//   
+
